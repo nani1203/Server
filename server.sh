3cc7cffb5f2003b4d8119c6056843ea129f07d0a
nani1203 <89770120+nani1203@users.noreply.github.com>
2023-05-25 16:44:59 +0530

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Check URL') {
+            steps {
+                script {
+                    def url = 'https://www.youtube.com/'  // Specify the URL you want to check
+
+                    def response = httpRequest(url: url, ignoreSslErrors: true)
+                    
+                    if (response.status == 200) {
+                        println "URL ${url} is reachable."
+                    } else {
+                        println "URL ${url} is not reachable. Response status: ${response.status}"
+                    }
+                }
+            }
+        }
+    }
+}
